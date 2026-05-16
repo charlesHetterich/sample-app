a6b01ea4bd638586801341cde06dec6cae61f885
Charles Hetterich <hetterich.charles@gmail.com>
2026-05-15 22:21:32 -0400

    updates

--- hardhat-counter/contracts/CounterB.sol
+++ hardhat-counter/contracts/CounterB.sol
@@ -6,7 +6,7 @@ interface ICounterA {
     function increment() external returns (uint256);
     function add(uint256 amount) external returns (uint256);
 }
-
+/// @custom:cdm @example/counter-b
 contract CounterB {
     address public counterA;
     uint256 public localCount;
