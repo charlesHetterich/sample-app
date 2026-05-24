04217f0d76ee7f1a72dd557602536a2690229614
Charles Hetterich <hetterich.charles@gmail.com>
2026-05-24 17:07:59 -0400

    reset projects

--- foundry-counter/setup.sh
+++
@@ -1,1 +0,0 @@
-printf "\n\033[1mRun \`cdm build\` to compile the Solidity contracts.\033[0m\n"
--- hardhat-counter/setup.sh
+++
@@ -1,2 +0,0 @@
-pnpm install
-printf "\n\033[1mRun \`cdm build\` to compile the Solidity contracts.\033[0m\n"
